aa7f4543b10cb4b3354ebe3a408c2a04839c40d9
DaniaProgrammer765 <daniaalsmman037@gmail.com>
2025-11-08 14:04:40 +0300

    edit footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,41 +12,39 @@ export default function Footer() {
   return (
     <footer className={`w-full text-gray-700 pt-12 pb-4 border-t border-gray-100 ${isMain ? `bg-white`: `bg-[#FBF9F9]`}`}>
       <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-4 gap-10 text-right">
-        {/* العمود 1:  العنوان */}
-        <div className="text-right">
-          <h3 className="relative font-semibold text-primary mb-5 inline-block">
-            العنوان
-            <span className="absolute bottom-0 right-0 translate-y-3 w-[55%] h-[2px] bg-pink-600 rounded-full"></span>
-          </h3>
-          <ul className="space-y-3 text-sm mt-2">
-            <li className="flex items-start gap-2 ">
+        {/* العمود 1:  الشعار و النص */}
+        <div className="flex flex-col">
+          <Image
+            src="/logo.svg"
+            alt="YallaCheese Logo"
+            width={95}
+            height={28}
+            className="mb-4"
+          />
+          <p className="text-sm leading-relaxed text-gray-600 text-right">
+            تأسست YallaCheese في الأردن عام 2018 وهي خدمة طباعية عبر الإنترنت
+            تقدم جودة ممتازة للزبائن في حاجة لمطبوعات احترافية.
+            <br /> عمان - الأردن
+          </p>
+          <div className="flex  gap-3 mt-4">
+            <Link href="#">
               <Image
-                src="/icons/location.svg"
-                alt="Location"
-                width={16}
-                height={16}
+                src="/icons/facebook.svg"
+                alt="Facebook"
+                width={30}
+                height={30}
               />
-              <span>
-                الطابق 27 شارع فيفيث، نيويورك، نيويورك، 10002، الولايات المتحدة
-                الأمريكية
-              </span>
-            </li>
-            <li className="flex items-center gap-2 ">
-              <Image src="/icons/mail.svg" alt="Email" width={16} height={16} />
-              <span>البريد الإلكتروني</span>
-            </li>
-            <li className="flex items-center gap-2 ">
+            </Link>
+            <Link href="#">
               <Image
-                src="/icons/phone.svg"
-                alt="Phone"
-                width={16}
-                height={16}
+                src="/icons/instagram.svg"
+                alt="Instagram"
+                width={30}
+                height={30}
               />
-              <span>الهاتف</span>
-            </li>
-          </ul>
+            </Link>
+          </div>
         </div>
-
         {/* العمود 2: الصفحات */}
         <div className="text-right">
           <h3 className="relative font-semibold text-primary mb-5 inline-block">
@@ -100,38 +98,40 @@ export default function Footer() {
           </ul>
         </div>
 
-        {/* العمود 4: الشعار والنص  */}
-        <div className="flex flex-col">
-          <Image
-            src="/logo.svg"
-            alt="YallaCheese Logo"
-            width={95}
-            height={28}
-            className="mb-4"
-          />
-          <p className="text-sm leading-relaxed text-gray-600 text-right">
-            تأسست YallaCheese في الأردن عام 2018 وهي خدمة طباعية عبر الإنترنت
-            تقدم جودة ممتازة للزبائن في حاجة لمطبوعات احترافية.
-            <br /> عمان - الأردن
-          </p>
-          <div className="flex  gap-3 mt-4">
-            <Link href="#">
+        {/* العمود 4:  العنوان  */}
+        
+        <div className="text-right">
+          <h3 className="relative font-semibold text-primary mb-5 inline-block">
+            العنوان
+            <span className="absolute bottom-0 right-0 translate-y-3 w-[55%] h-[2px] bg-pink-600 rounded-full"></span>
+          </h3>
+          <ul className="space-y-3 text-sm mt-2">
+            <li className="flex items-start gap-2 ">
               <Image
-                src="/icons/facebook.svg"
-                alt="Facebook"
+                src="/icons/location.svg"
+                alt="Location"
                 width={20}
                 height={20}
               />
-            </Link>
-            <Link href="#">
+              <span>
+                الطابق 27 شارع فيفيث، نيويورك، نيويورك، 10002، الولايات المتحدة
+                الأمريكية
+              </span>
+            </li>
+            <li className="flex items-center gap-2 ">
+              <Image src="/icons/mail.svg" alt="Email" width={20} height={20} />
+              <span>البريد الإلكتروني</span>
+            </li>
+            <li className="flex items-center gap-2 ">
               <Image
-                src="/icons/instagram.svg"
-                alt="Instagram"
+                src="/icons/phone.svg"
+                alt="Phone"
                 width={20}
                 height={20}
               />
-            </Link>
-          </div>
+              <span>الهاتف</span>
+            </li>
+          </ul>
         </div>
       </div>
 
